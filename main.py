e2c7c3639ece3117a733fe08dd948ff01a3b86ba
lamyh4242-ship-it <lamyh4242@gmail.com>
2026-08-20 01:17:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ def read_root():
     return {"message": "Le serveur de scraping Bet261 est en ligne !"}
 
 @app.get("/dashboard")
-async def get_dashboard():
-    # On cible l'endpoint des plannings/rondes pour cette catégorie
-    target_api = "https://hg-event-api-prod.sporty-tech.net/api/instantleagues/schedules?eventCategoryId=1616288&parentEventCategoryId=8035"
+async def get_dashboard(round_num: int = 34):
+    # URL exacte récupérée de tes captures
+    target_api = f"https://hg-event-api-prod.sporty-tech.net/api/instantleagues/round/{round_num}?eventCategoryId=1616288&getNext=false"
     
+    # Headers complets incluant l'App-Version indispensable
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36",
+        "App-Version": "34727",
         "Origin": "https://bet261.mg",
         "Referer": "https://bet261.mg/",
         "Accept": "application/json, text/plain, */*"
@@ -29,6 +31,7 @@ async def get_dashboard():
                 data_content = r.text[:1000]
             
             return {
+                "ronde_testee": round_num,
                 "status_http": r.status_code,
                 "content_type": r.headers.get("content-type"),
                 "data": data_content
